2963a4a7e170313b5756a2c3563aa00acf6d6b73
EfeErkan <e.erkan@ug.bilkent.edu.tr>
2023-09-28 18:58:29 +0300

    Initial setup

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+
+# Create a Flask application
+app = Flask(__name__)
+
+# Define a route that returns a JSON response
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {
+        'message': 'Hello from the Flask backend!',
+        'data': [1, 2, 3, 4, 5]
+    }
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
