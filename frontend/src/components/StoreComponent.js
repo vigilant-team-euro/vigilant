2ecd6c3c3c197c508c418fce1d19b6fe8128f779
OnurAsimIlhan <63584455+OnurAsimIlhan@users.noreply.github.com>
2023-11-26 15:44:07 +0300

    clientpage fix

--- frontend/src/components/StoreComponent.js
+++ frontend/src/components/StoreComponent.js
@@ -1,14 +1,59 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faLock, faUnlock, faDesktop } from "@fortawesome/free-solid-svg-icons";
 
-const StoreComponent = ({ title, type, data, options, height }) => {
+const Card2 = ({ cardTitle, cardContent, opacity, type }) => {
+  const cardStyle = {
+    position: "relative",
+    border: "1px solid #ddd",
+    borderRadius: "8px",
+    padding: "16px",
+    margin: "20px",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+    backgroundColor: "#2d404f",
+    opacity: opacity || 1,
+  };
+
+  const titleStyle = {
+    fontSize: "16px",
+    fontWeight: "bold",
+    marginBottom: "8px",
+    color: "#fff",
+  };
+
+  const contentStyle = {
+    fontSize: "14px",
+    color: "#fff",
+  };
+
+  const iconStyle = {
+    position: "absolute",
+    top: "50%",
+    right: "16px",
+    transform: "translateY(-50%)",
+    color: "#fff",
+    fontSize: "2em"
+  };
+
+  return (
+    <div style={cardStyle}>
+      
+      <div style={titleStyle}>{cardTitle || "Card Title"}</div>
+      <div style={contentStyle}>{cardContent || "Card Content"}</div>
+      <FontAwesomeIcon icon={type} style={iconStyle} />
+      
+    </div>
+  );
+};
+
+const StoreComponent = ({ title, type, height }) => {
   const cardStyle = {
     border: "1px solid #ddd",
     borderRadius: "8px",
     padding: "16px",
     margin: "8px",
     boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-    backgroundColor: "#262134", // Set the background color
+    backgroundColor: "#262134",
     height: height,
   };
 
@@ -16,15 +61,18 @@ const StoreComponent = ({ title, type, data, options, height }) => {
     fontSize: "18px",
     fontWeight: "bold",
     marginBottom: "16px",
-    color: "white", // Set the font color to white
-    textAlign: "center", // Center the title
+    color: "white",
+    textAlign: "center",
   };
 
   return (
     <div style={cardStyle}>
       <div style={titleStyle}>{title || "Default Title"}</div>
-
-      
+      <Card2 cardTitle="Heatmap Analysis" cardContent="Detailed analysis of customers walking path, visualized in heatmap" type={faCheck}/>
+      <Card2 cardTitle="Camera Settings" cardContent="Adding, removing cameras and video footage" type={faDesktop} />
+      <Card2 cardTitle="Employee List" cardContent="Adding, removing employees to system" type={faDesktop} />
+      <Card2 cardTitle="Forcasting" cardContent="Forecasting the future" type={faLock} opacity={0.5} />
+      <Card2 cardTitle="Payment Records" cardContent="Records of payments obtained from shop" type={faLock} opacity={0.5} />
     </div>
   );
 };
