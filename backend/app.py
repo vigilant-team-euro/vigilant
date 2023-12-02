396058962d6ae99f4fe534a0b4fabf42e685bb9c
EfeErkan <e.erkan@ug.bilkent.edu.tr>
2023-12-02 11:28:26 +0300

    Video processing backend base
    HTTP Post request handled

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 # Create a Flask application
 app = Flask(__name__)
@@ -11,6 +11,15 @@ def get_data():
         'data': [1, 2, 3, 4, 5]
     }
     return jsonify(data)
+
+@app.route('/api/video', methods=['POST'])
+def process_video():
+    if 'video' in request.files:
+        video_file = request.files['video']
+        # Do something with the video here
+        return {'message': 'Video uploaded successfully'}, 200
+    else:
+        return {'error': 'No video file provided'}, 400
 
 if __name__ == '__main__':
     app.run(debug=True)
