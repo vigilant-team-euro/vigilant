1142b0b28bcdac022b93922da8ac0c61353646ad
uygaronaterol <uygar.1212@hotmail.com>
2024-03-17 03:32:23 +0300

    backend frontend connected
    Fetched data from backend to forecast page. There are some issues still, it repeatedly sends the object.

--- new/src/pages/forecast/ForecastPage.js
+++ new/src/pages/forecast/ForecastPage.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import "./forecastpage.scss"
 function ForecastPage() {
+  let  [forecast, setForecast] = useState([])
+  useEffect(()=>{
+          getData()
+      }, [forecast])
+      let getData = async ()=>{
+          let response = await  fetch('http://127.0.0.1:5000/get_store_data',{
+              method:'GET',
+              headers:{
+                  'Content-Type':'application/json',
+                
+              }
+          })
+          let data = await response.json()
+          setForecast(data)
+  }
+  console.log(forecast)
   return (
+    
     <div className="forecast">
       <div className="box box1">Graph Settings
-      
       </div>
       <div className="box box2">2
       
